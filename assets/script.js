0532567a73f31d9f122517c219f46e09682d949b
Kafela-101 <civilianforces101@gmail.com>
2026-03-25 23:02:30 +0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,8 @@
-h
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+}
